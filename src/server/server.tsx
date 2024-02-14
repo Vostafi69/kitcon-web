3f5376050ea4fccf1e2985e7cb140c90932a4917
Alexander <thorik.sanya@gmail.com>
2024-02-14 14:10:13 +1000

    server init

---
+++ src/server/server.tsx
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as path from "path";
+import * as mustacheExpress from "mustache-express";
+
+const app = express();
+
+const currentPath = path.join(global.ROOT_PATH, "dist", "server");
+const publicPath = path.join(global.ROOT_PATH, "dist", "public");
+
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", currentPath + "/views");
+
+app.use(express.static(publicPath));
+
+app.get("./favicon.ico", (req, res) => res.status(500).end());
+
+app.get("*", async (req: express.Request, res: express.Response) => {
+  res.render("index", {});
+});
+
+app.listen(process.env.PORT, () => {
+  console.info(`application started at ${process.env.PORT} port`);
+});
